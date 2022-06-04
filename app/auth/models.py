305cb213d4afaf058092a48f52ee61c588b63386
Gregorio Zamora <jgregoza@outlook.com>
2022-06-04 10:05:50 -0600

    Column error:ModelUser

--- app/auth/models.py
+++ app/auth/models.py
@@ -4,10 +4,10 @@ from app import login_manager
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
-    id = db.Colum(db.Integer, primary_key=True)
-    user_name = db.Colum(db.String(20))
-    user_email = db.Colum(db.String(60), unique=True, index=True)
-    user_password = db.Colum(db.String(70))
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(20))
+    user_email = db.Column(db.String(60), unique=True, index=True)
+    user_password = db.Column(db.String(70))
 
     def check_password(self, password):
         return bcrypt.check_password_hash(self.user_password, password)
